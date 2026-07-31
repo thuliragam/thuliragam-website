8b4ecbec3042066cfe970001dfb9b0ff95ffdb44
thuliragam <thuliragam.in@gmail.com>
2026-07-31 15:41:07 +0530

    Fix sitemap domain to .in

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.thuliragam.com";
+ const baseUrl = "https://thuliragam.in";
 
   return [
     {
